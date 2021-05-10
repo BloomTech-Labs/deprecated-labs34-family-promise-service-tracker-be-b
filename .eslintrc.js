dc4d415ad1d9376f29f84cbd039999c1bb11a542
Remy Vila <josephvila123@gmail.com>
2021-05-10 12:05:53 -0400

    dev env config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,12 @@
 module.exports = {
-  env: {
-    node: true,
-    commonjs: true,
-    es2020: true,
-  },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-  parserOptions: {
-    ecmaVersion: 11,
-  },
-  plugins: ['prettier'],
-  rules: {},
-  overrides: [
-    {
-      files: ['__tests__/*', '__tests__/**/*'],
-      env: {
-        jest: true,
-      },
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2020": true
     },
-  ],
+    "parserOptions": {
+        "ecmaVersion": 11
+    },
+    "rules": {
+    }
 };
