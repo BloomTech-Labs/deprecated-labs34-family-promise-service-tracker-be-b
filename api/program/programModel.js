05acba7bd1b81d290b8f30e5a153d5d11390d650
Remy Vila <josephvila123@gmail.com>
2021-05-19 19:54:10 -0400

    C:/Program Files/Git/programs/profile/id

--- api/program/programModel.js
+++ api/program/programModel.js
@@ -1,47 +1,41 @@
 const knex = require('../../data/db-config');
 
 const findAll = async () => {
-  return await knex('programs')
-    .leftJoin('programs_users', {
-      'programs.id': 'programs_users.program_id',
-    })
-    .leftJoin('profiles', {
-      'programs_users.profile_id': 'profiles.id',
-    })
-    .select(knex.raw('programs.*, json_agg(profiles.*) as users'))
-    .groupBy('programs.id');
+  return await knex('programs');
 };
 
-const findById = async (id) => {
-  return await knex('programs')
-    .leftJoin('programs_users', {
-      'programs.id': 'programs_users.program_id',
-    })
-    .leftJoin('profiles', {
-      'programs_users.profile_id': 'profiles.id',
-    })
-    .select(knex.raw('programs.*, json_agg(profiles.*) as users'))
-    .where({ 'programs.id': id })
-    .groupBy('programs.id')
-    .first();
+const findById = (id) => {
+  return knex('programs').where({ program_id: id });
 };
 
-const findBy = async (filter) => {
-  return await knex('programs')
-    .leftJoin('programs_users', {
-      'programs.id': 'programs_users.program_id',
-    })
-    .leftJoin('profiles', {
-      'programs_users.profile_id': 'profiles.id',
-    })
-    .select(knex.raw('programs.*, json_agg(profiles.*) as users'))
-    .where(filter)
-    .groupBy('programs.id')
-    .first();
+const findByProfileId = async (profile_id) => {
+  // need access to current logged in profile
+  /*
+    use that profile_id to ask Programs_Managers...
+    which programs it is related to
+  */
+  const p_mTable = await knex('program_managers').where({
+    program_manager_id: profile_id,
+  });
+  // use forEach to return all profile_id's
+  let idArr = [];
+  p_mTable.forEach((id) => {
+    idArr.push(id.program_id);
+  });
+
+  // return from Programs programs that...
+  // match any of the numbers in the forEach
+
+  return knex('programs').whereIn('program_id', idArr);
+
+  // return knex('programs').where({ program_id: p_mTable[0].program_id });
 };
+
+const deleteById = async (id) => {};
 
 module.exports = {
   findAll,
   findById,
-  findBy,
+  findByProfileId,
+  deleteById,
 };
